8623c7b96533414ffc3fa5c66f14646f60e6b4ec
Luke <39779310+ClashLuke@users.noreply.github.com>
2020-08-08 15:01:59 +0200

    feat: add multiprocessing

--- download_all_fx_data.py
+++ download_all_fx_data.py
@@ -1,5 +1,6 @@
 import csv
 import datetime
+import multiprocessing
 import os
 import sys
 import time
@@ -28,39 +29,46 @@ class RateTracker:
         return (time.time() - self.start_time) / self.instances
 
 
+def process_currency(row, start_year):
+    currency_pair_name, pair, history_first_trading_month = row
+    year = int(history_first_trading_month[0:4])
+    sys.__stdout__.write(f'Started {currency_pair_name}')
+    output_folder = os.path.join('output', pair)
+    mkdir_p(output_folder)
+    local_rate = RateTracker()
+    total_years = start_year - year
+    try:
+        while True:
+            try:
+                download_hist_data(year=year, pair=pair,
+                                   output_directory=output_folder)
+            except AssertionError:
+                month = 1
+                while month <= 12:
+                    download_hist_data(year=str(year), month=str(month),
+                                       pair=pair,
+                                       output_directory=output_folder)
+                    month += 1
+            year += 1
+            sys.__stdout__.write(f'[{currency_pair_name}]'
+                                 f'[{local_rate.instances}/{total_years}] '
+                                 f'Current: {local_rate():.2f}s/Year')
+    except Exception:
+        pass
+    sys.__stdout__.write(f"FINISHED {currency_pair_name} - {year} years downloaded")
+
+
 def download_all():
     sys.stdout = open("/dev/null", 'w')
-    start_year, global_rate = datetime.datetime.now().year, RateTracker()
+    start_year = datetime.datetime.now().year
     with open('pairs.csv', 'r') as f:
         reader = csv.reader(f, delimiter=',')
         next(reader, None)  # skip the headers
-        for row in reader:
-            currency_pair_name, pair, history_first_trading_month = row
-            year = int(history_first_trading_month[0:4])
-            sys.__stdout__.write(f'Started {currency_pair_name}')
-            output_folder = os.path.join('output', pair)
-            mkdir_p(output_folder)
-            local_rate = RateTracker()
-            total_years = start_year - year
-            try:
-                while True:
-                    try:
-                        download_hist_data(year=year, pair=pair,
-                                           output_directory=output_folder)
-                    except AssertionError:
-                        month = 1
-                        while month <= 12:
-                            download_hist_data(year=str(year), month=str(month),
-                                               pair=pair,
-                                               output_directory=output_folder)
-                            month += 1
-                    year += 1
-                    sys.__stdout__.write(f'\r[{currency_pair_name}]'
-                                         f'[{local_rate.instances}/{total_years}] '
-                                         f'Current: {local_rate():.2f}s/Year - '
-                                         f'Total: {global_rate(): .2f}s/Year')
-            except Exception:
-                sys.__stdout__.write('\n')
+        pool = [multiprocessing.Process(target=process_currency, args=(row, start_year)) for row in reader]
+    for p in pool:
+        p.start()
+    for p in pool:
+        p.join()
     sys.stdout = sys.__stdout__
 
 
